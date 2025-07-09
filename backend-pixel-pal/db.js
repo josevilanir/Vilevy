1fc5a844e6ff5a10ac6995a07cd2802efb4e7cfa
josevilanir <vilanirneto@hotmail.com>
2025-07-09 19:01:08 -0300

    CHORE:  Mudança em banco para deploy

--- backend-pixel-pal/db.js
+++ backend-pixel-pal/db.js
@@ -1,17 +1,6 @@
 import { Pool } from 'pg';
 
-const {
-  DB_HOST = 'localhost',
-  DB_PORT = '5432',
-  DB_USER = 'postgres',
-  DB_PASS = 'postgres',
-  DB_NAME = 'pixelpal',
-} = process.env;
-
 export const pool = new Pool({
-  host: DB_HOST,
-  port: parseInt(DB_PORT, 10),
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
